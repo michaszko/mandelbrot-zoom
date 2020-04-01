ce22464b66b5b7f9b9b5686d530c589c00520960
michaszko <michal96.96@gmail.com>
2020-04-01 17:00:22 +0200

    What Ive already done

---
+++ Fraktal.cpp
@@ -0,0 +1,178 @@
+//**************************
+// Mandelbrot Fractal
+//**************************
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+// Resolution of image
+const int W = 1920;
+const int H = 1080;
+
+// Maximal color
+const int alpha = 255;
+// Maximal iteration
+const int max_iteration = 10000;
+// zoom factor
+const long double s = 0.97;
+
+long double ss;
+long double kappa;
+int sigma;
+
+// Position of point we are zooming in
+// long double X = -0.789374599271466936740382412558;
+// long double Y = 0.163089252677526719026415054868;
+long double X = -1.477110786384222313461222586803179083557;
+long double Y = 0.003322002718062184557764259218386616609;
+
+// Pixel structure - position and color
+struct Pixel {
+  long double x;
+  long double y;
+  int color_r;
+  int color_g;
+  int color_b;
+};
+
+// Iteration of one pixel
+int mandelbrot(long double x, long double y, int max_iteration) {
+  long double tmp;
+  int iteration = 0;
+
+  // remeber starting value
+  long double x0 = x;
+  long double y0 = y;
+
+  // While |x-iy|<4 and we haven't reach maximum iteration yet...
+  while (x * x + y * y < 2 * 2 && iteration < max_iteration) {
+    tmp = x * x - y * y + x0;
+    // ...apply transformation:
+    // x -> x^2 - y^2 + x0
+    // y -> 2xy + y0
+    y = 2 * x * y + y0;
+    x = tmp;
+    iteration++;
+  }
+
+  // if maximal iteration was crossed (so point converges) then the pixel is
+  // black
+  if (iteration >= max_iteration) return 0;
+
+  // if not then return iteration after which convergance is broken (color will
+  // depend on that values)
+  else
+    return iteration;
+}
+
+// xmin           xmax
+//  |              |
+//  V              V
+//  ---------------- <- ymax
+//  |      /---\   |
+//  | -o--|     |  |
+//  |      \---/   |
+//  ---------------- <- ymin
+//
+// Plot one frame (whole Mandelbort set)
+void mandelbrotset(string fileName, long double xmin, long double xmax,
+                   long double ymin, long double ymax) {
+  ofstream file;
+
+  file.open(fileName.c_str());
+
+  // Insert header
+  file << "P3" << endl;
+  file << "# Fraktal Mandelbota" << endl;
+  file << W << " " << H << endl;
+  file << alpha << endl;
+
+  // Width and height of one pixel
+  long double dW = (xmax - xmin) / (long double)W;
+  long double dH = (ymax - ymin) / (long double)H;
+
+  // For each pixel ...
+  for (long double j = 0; j < H; j++)
+    for (long double i = 0; i < W; i++) {
+      Pixel pixel;
+      int iteration;
+
+      // calculate the coordinates of a pixel
+      pixel.x = xmin + (dW * i);
+      pixel.y = ymin + (dH * j);
+
+      // calculate if it converges
+      iteration = mandelbrot(pixel.x, pixel.y, max_iteration);
+
+      // play with color (here you can try your ideas, personally I dont have
+      // idea how to predict what Ill get)
+      kappa = (double)iteration / (double)max_iteration;
+      sigma = static_cast<int>(alpha * kappa);
+
+      // if (kappa < 0.33)
+      // {
+      pixel.color_r = sigma % 2 * 255;
+      pixel.color_g = sigma % 4 * 64;
+      pixel.color_b = sigma % 8 * 32;
+      // }
+
+      // else if (kappa >= 0.33 and kappa < 0.66)
+      // {
+      // 	pixel.color_r = sigma % 4 * 64;
+      // 	pixel.color_g = sigma % 4 * 64;
+      // 	pixel.color_b = sigma % 8 * 32;
+      // }
+
+      // else
+      // {
+      // 	pixel.color_r = sigma % 8 * 32;
+      // 	pixel.color_g = sigma % 8 * 32;
+      // 	pixel.color_b = sigma % 16 * 16;
+      // }
+
+      // save pixel to file
+      file << pixel.color_r << " " << pixel.color_g << " " << pixel.color_b
+           << endl;
+    }
+
+  file.close();
+}
+
+int main(int argc, char *argv[]) {
+  ostringstream name;
+
+  // first and last frame number (1 is maximally zoomed out, the higher number
+  // gets the more zoomed it is; it breaks around 3000s)
+  int begining = atoi(argv[1]);
+  int end = atoi(argv[2]);
+
+  // initial zoom
+  ss = pow(s, begining);
+
+  for (int i = begining; i < end; i++) {
+    // zoom chcnges by zooming factor
+    ss *= s;
+
+    name.str("");
+
+    if (i < 10) {
+      name << "Pic/Fraktal_000" << i << ".ppm";
+    } else if (i < 100) {
+      name << "Pic/Fraktal_00" << i << ".ppm";
+    } else if (i < 1000) {
+      name << "Pic/Fraktal_0" << i << ".ppm";
+    }
+
+    cout << "Just made " << i << " fractal\n";
+
+    // create frame
+    mandelbrotset(name.str(), X - ss, X + ss, Y - ss, Y + ss);
+  }
+
+  return 0;
+}
---
+++ Mandelbrot.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Script to produce frames and collect them and create movie
+# Prereiquirements: 
+# -> imagemagick (for mogrify)
+# -> ffmpeg (to create a video)
+
+SECONDS=0
+STEP=100
+
+# Directory for pictures and movie
+mkdir -p Pic
+
+# Compile program
+g++ Fraktal.cpp -o Fraktal.o
+
+# Create pictures in groups of $STEP 
+for i in {1...10}; do
+
+	echo "Create frames from "$(($i*$STEP))" till "$((($i+1)*$STEP))"..."
+	# Arguments in order - 
+	# 1) Number of first frame
+	# 2) Number of second frame
+	./Fraktal.o $(($i*$STEP)) $((($i+1)*$STEP)) 
+	
+	echo "Converting .ppm to .jpg..."
+	mogrify -format jpg Pic/*.ppm
+	
+	echo "Remove files .ppm..."
+	rm Pic/*.ppm
+	
+	ALL=$(($ALL + $SECONDS))
+	duration=$SECONDS
+	echo -e "\e[31mThis part took me "$(($duration / 60))" minutes and "$(($duration % 60))" seconds.\e[0m"
+	SECONDS=0
+	
+	done
+
+echo "Making movie..."
+ffmpeg -r 30 -i Pic/Fraktal_%4d.jpg -qscale 1 Pic/Mandelbrot.mp4
+
+echo -e "\e[32mWhole thing took me "$(($ALL / 60))" minutes and "$(($ALL % 60))" seconds.\e[0m"
